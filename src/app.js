bc7a957a091a9d9bc83e7065e370cf3287548816
Charlotte Gardner <ckgardner87@hotmail.com>
2021-06-19 11:14:19 +0100

    improve styling and add apiKey and Url in javascript

--- src/app.js
+++ src/app.js
@@ -1,1 +1,5 @@
-
+let apiKey = "9b9238de779b229d195c2b6f0ba8b479";
+let apiEndpoint = `https://api.openweathermap.org/data/2.5/`;
+let units = "Metric";
+let apiUrl = `${apiEndpoint}find?q=Poole&units=${units}&appid=${apiKey}`;
+console.log(apiUrl);
