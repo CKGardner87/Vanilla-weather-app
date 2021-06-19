e74478607b33116dc0069357c7cbe119f64fa7e2
Charlotte Gardner <ckgardner87@hotmail.com>
2021-06-19 16:25:18 +0100

    add javascript function to update current conditions

--- src/app.js
+++ src/app.js
@@ -32,6 +32,8 @@ let currentTemp = document.querySelector("#current-temp");
 let humid = document.querySelector("#humidity");
 let condition = document.querySelector("#description");
 let feels = document.querySelector("#feels-like");
+let minTemp = document.querySelector("#today-low");
+let maxTemp = document.querySelector("#today-high");
 let apiKey = "9b9238de779b229d195c2b6f0ba8b479";
 let apiEndpoint = `https://api.openweathermap.org/data/2.5/`;
 let units = "Metric";
@@ -45,9 +47,13 @@ function showTemperature(response) {
   let humidityLevel = response.data.list[0].main.humidity;
   let tempFeels = Math.round(response.data.list[0].main.feels_like);
   let weatherDescrip = response.data.list[0].weather[0].main;
+  let highTemp = Math.round(response.data.list[0].main.temp_max);
+  let lowTemp = Math.round(response.data.list[0].main.temp_min);
 
   currentTemp.innerHTML = `${temperature}`;
   feels.innerHTML = `Feels like: ${tempFeels}`;
   humid.innerHTML = `Humidity: ${humidityLevel}%`;
   condition.innerHTML = `Currently: ${weatherDescrip}`;
+  maxTemp.innerHTML = `${highTemp}`;
+  minTemp.innerHTML = `${lowTemp}`;
 }
